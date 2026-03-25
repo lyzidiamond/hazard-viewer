c88d348e78ee560ac5b340efaeab72b4b2ecc966
Lyzi Diamond <lyzidiamond@gmail.com>
2026-03-24 21:57:04 -0400

    try http for VITE_API_URL

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,5 +1,5 @@
 const API_URL = import.meta.env.VITE_API_URL
-  ? `https://${import.meta.env.VITE_API_URL}`
+  ? `http://${import.meta.env.VITE_API_URL}`
   : "http://localhost:8000";
 
 // throws error on non-200 responses
